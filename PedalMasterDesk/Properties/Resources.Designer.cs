1fa849e7e86a1f33507896f5a20ce552a4869923
IGOR DE OLIVEIRA MELO <igor.omelo1@senacsp.edu.br>
2024-11-08 11:46:54 -0300

    Imagens Para DataGridView de Formulario Pedidos

--- PedalMasterDesk/Properties/Resources.Designer.cs
+++ PedalMasterDesk/Properties/Resources.Designer.cs
@@ -69,5 +69,25 @@ namespace PedalMasterDesk.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Consulta um recurso localizado do tipo System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap botao_mais {
+            get {
+                object obj = ResourceManager.GetObject("botao_mais", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Consulta um recurso localizado do tipo System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap lixeira {
+            get {
+                object obj = ResourceManager.GetObject("lixeira", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
